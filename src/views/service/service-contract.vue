c4c9491688406dcf6e04d1d9d4c6cf17f6417b64
czh740132583 <740132583@qq.com>
2022-01-04 18:17:12 +0800

    1.智能合约服务修改

--- src/views/service/service-contract.vue
+++ src/views/service/service-contract.vue
@@ -131,7 +131,9 @@
                 </div>
             </div>
             <div class="table-text">
-                <img src="../../assets/img/white-box.png" height="241" width="420"/>
+                <img v-show = "tableChoseBox === 1" src="../../assets/img/white-box.png" height="241" width="420"/>
+                <img v-show = "tableChoseBox === 2" src="../../assets/img/gary-box.png" height="241" width="420"/>
+                <img v-show = "tableChoseBox === 3" src="../../assets/img/black-box.png" height="241" width="420"/>
                 <div class="table-introduce">
                     <span>
                         {{boxText}}
@@ -227,42 +229,26 @@
                                 if (rowIndex === 0) {
                                     return 7
                                 }
-                                if (rowIndex === 7) {
+                                if (rowIndex === 7 || rowIndex === 10 || rowIndex === 19 || rowIndex === 22) {
                                     return 3
                                 }
-                                if (rowIndex === 10) {
-                                    return 3
-                                }
+
                                 if (rowIndex === 13) {
                                     return 6
-                                }
-                                if (rowIndex === 19) {
-                                    return 3
-                                }
-                                if (rowIndex === 22) {
-                                    return 3
                                 }
                                 if (rowIndex === 25){
                                     return 9
                                 }
-                                if (rowIndex === 34){
+                                if (rowIndex === 34 || rowIndex === 35) {
                                     return 1
                                 }
-                                if (rowIndex === 35){
-                                    return 1
-                                }
+
                             }
                             if(tableChoseBox.value === 2){
-                                if(rowIndex === 0 ){
+                                if(rowIndex === 0 || rowIndex === 19){
                                     return 1
                                 }
-                                if (rowIndex === 1 ) {
-                                    return 3
-                                }
-                                if (rowIndex === 4) {
-                                    return 3
-                                }
-                                if (rowIndex === 12) {
+                                if (rowIndex === 1 || rowIndex === 4 || rowIndex === 12) {
                                     return 3
                                 }
                                 if (rowIndex === 7) {
@@ -270,9 +256,6 @@
                                 }
                                 if (rowIndex === 15) {
                                     return 4
-                                }
-                                if (rowIndex === 19) {
-                                    return 1
                                 }
                             }
                             if(tableChoseBox.value === 3){
@@ -359,92 +342,12 @@
                     },
                 ]
             }
-            function rowClassName (row:object,index:number) {
-                if(row.num%2 === 0){
+            function rowClassName (row: {num:string,categories:string }) {
+                if(Number(row.num) % 2 === 0){
                     return 'change-color'
                 }
                 return null
             }
-            /*const whiteBox = <Array<Object>>[
-                {num:'01',categories:t('lang.serviceContract.table.white.categories01'),items:t('lang.serviceContract.table.white.item01.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item01.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item01.text03')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item01.text04')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item01.text05')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item01.text06')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item01.text07')},
-                {num:'02',categories:t('lang.serviceContract.table.white.categories02'),items:t('lang.serviceContract.table.white.item02.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item02.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item02.text03')},
-                {num:'03',categories:t('lang.serviceContract.table.white.categories03'),items:t('lang.serviceContract.table.white.item03.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item03.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item03.text03')},
-                {num:'04',categories:t('lang.serviceContract.table.white.categories04'),items:t('lang.serviceContract.table.white.item04.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item04.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item04.text03')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item04.text04')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item04.text05')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item04.text06')},
-                {num:'05',categories:t('lang.serviceContract.table.white.categories05'),items:t('lang.serviceContract.table.white.item05.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item05.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item05.text03')},
-                {num:'06',categories:t('lang.serviceContract.table.white.categories06'),items:t('lang.serviceContract.table.white.item06.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item06.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item06.text03')},
-                {num:'07',categories:t('lang.serviceContract.table.white.categories07'),items:t('lang.serviceContract.table.white.item07.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text03')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text04')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text05')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text06')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text07')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text08')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.white.item07.text09')},
-                {num:'08',categories:t('lang.serviceContract.table.white.categories08'),items:t('lang.serviceContract.table.white.item08')},
-                {num:'09',categories:t('lang.serviceContract.table.white.categories09'),items:t('lang.serviceContract.table.white.item09')},
-            ]*/
-            const greyBox = <Array<Object>>[
-                {num:'01',categories:t('lang.serviceContract.table.grey.categories01'),items:t('lang.serviceContract.table.grey.item01')},
-                {num:'02',categories:t('lang.serviceContract.table.grey.categories02'),items:t('lang.serviceContract.table.grey.item02.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item02.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item02.text03')},
-                {num:'03',categories:t('lang.serviceContract.table.grey.categories03'),items:t('lang.serviceContract.table.grey.item03.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item03.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item03.text03')},
-                {num:'04',categories:t('lang.serviceContract.table.grey.categories04'),items:t('lang.serviceContract.table.grey.item04.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item04.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item04.text03')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item04.text04')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item04.text05')},
-                {num:'05',categories:t('lang.serviceContract.table.grey.categories05'),items:t('lang.serviceContract.table.grey.item05.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item05.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item05.text03')},
-                {num:'06',categories:t('lang.serviceContract.table.grey.categories06'),items:t('lang.serviceContract.table.grey.item06.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item06.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item06.text03')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.grey.item06.text04')},
-                {num:'07',categories:t('lang.serviceContract.table.grey.categories07'),items:t('lang.serviceContract.table.grey.item07')},
-            ]
-            const blackBox = <Array<Object>>[
-                {num:'01',categories:t('lang.serviceContract.table.black.categories01'),items:t('lang.serviceContract.table.black.item01.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item01.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item01.text03')},
-                {num:'02',categories:t('lang.serviceContract.table.black.categories02'),items:t('lang.serviceContract.table.black.item02.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item02.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item02.text03')},
-                {num:'03',categories:t('lang.serviceContract.table.black.categories03'),items:t('lang.serviceContract.table.black.item03.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item03.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item03.text03')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item03.text04')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item03.text05')},
-                {num:'04',categories:t('lang.serviceContract.table.black.categories04'),items:t('lang.serviceContract.table.black.item04.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item04.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item04.text03')},
-                {num:'04',categories:t('lang.serviceContract.table.black.categories05'),items:t('lang.serviceContract.table.black.item05.text01')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item05.text02')},
-                {num:'',categories:'',items:t('lang.serviceContract.table.black.item05.text03')},
-                {num:'06',categories:t('lang.serviceContract.table.black.categories06'),items:t('lang.serviceContract.table.black.item06')},
-            ]
             let boxText = computed<String>(()=>{
                 if(tableChoseBox.value === 1){
                     return t('lang.serviceContract.table.textWhite')
@@ -473,22 +376,6 @@
 
 <style scoped>
 @import "../../assets/css/service-page.css";
-
-:deep(.title) {
-  background-color: #FFF !important;
-}
-
-:deep(.title) :hover {
-  background-color: rgba(248, 248, 248, 0);
-}
-
-:deep(.categories) {
-  background-color: #FFF !important;
-}
-
-:deep(.categories) :hover {
-  background-color: #FFF !important;
-}
 
 /deep/.n-data-table-th{
   background-color: #F1F1F1 !important;
