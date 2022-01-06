fbb550dba76f7de5fe2f09804929b72b5a9e6751
czh740132583 <740132583@qq.com>
2022-01-06 17:49:27 +0800

    1.修復bug 4132

--- src/api/personal.ts
+++ src/api/personal.ts
@@ -39,38 +39,3 @@ export function verifyCode(params:IReportList){
     })
 }
 
-// http://192.168.0.32:40001/project/15/interface/api/265
-// 下载单个
-export interface IExpReport {
-    fileUuid?:string,
-    reportNum?:number
-}
-export function exportReport(params:IExpReport){
-    return request({
-        url: '/website/common/download/single',
-        method: 'get',
-        params,
-    })
-}
-// http://192.168.0.32:40001/project/15/interface/api/271
-// 下载全部
-export interface IExpReportAll {
-    belongUser?:boolean,
-}
-export function exportReportAll(params:IExpReportAll){
-    return request({
-        url: '/website/common/download/batch',
-        method: 'get',
-        params,
-    })
-}
-export interface ISearchReport {
-    num?:number,
-}
-export function searchReport(params:ISearchReport){
-    return request({
-        url: '/website/report/info',
-        method: 'get',
-        params,
-    })
-}
