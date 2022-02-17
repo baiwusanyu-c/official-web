38075c9062a34eb3fa3ec1f98ab5f74df9bc034c
czh740132583 <740132583@qq.com>
2022-02-17 19:32:43 +0800

    1.修復跳轉vaas 不登錄問題

--- src/views/product/product-vass.vue
+++ src/views/product/product-vass.vue
@@ -17,7 +17,7 @@
                 </div>
                  <img alt="" src="../../assets/img/vaas-logo.png" class="display-none w-36 h-36 mt-10 mx-auto sm:flex">
                 <be-button customClass="try-out-btn h-btn-txt-black linear-l-r text-black text-xl "
-                           @click="openWin(`https://vaas.lianantech.com/#/login?token=${getStore('token')}&lang=${getStore('lang')}`,'vaas')">
+                           @click="openWin(`http://124.71.193.230:17070/#/login?token=Bearer ${getStore('token')}&lang=${getStore('lang')}`,'vaas')">
                     <span class="font-format sm:text-base">{{ $t('lang.tryoutBtn') }}</span>
                 </be-button>
             </div>
