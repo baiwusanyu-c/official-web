8df3dc2ba075f62410d2ba6bcf16af43f1934cb3
czh740132583 <740132583@qq.com>
2022-03-15 15:10:10 +0800

    fix: 修复bug4442
    refactor: 优化删除了部分无用图标

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,14 @@ import 'virtual:windi.css'
 import BeUI from '../public/be-ui/be-ui.es'
 import './assets/css/be-ui/style.css'
 import { i18n } from './utils/i18n'
-
+import { SvgDom } from './utils/create-svg'
+/**
+ * 初始化图标，生成svgDom
+ */
+const initIcon = ():void =>{
+  new SvgDom()
+}
+initIcon()
 const app = createApp(App)
 app.use(BeUI)
 app.use(router)
