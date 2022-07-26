d05abd9df06881d6b57c361e59617c1e1418e112
wuwei <990030750@qq.com>
2022-07-26 10:45:37 +0800

    feat: 替换UI图片、添加meta标签

--- ssr/dev.mjs
+++ ssr/dev.mjs
@@ -17,20 +17,6 @@ const viteServer = await vite.createServer({
   },
 })
 
-/* relevant snippet from server.js */
-// const cssUrls = new Set(),
-const collectCssUrls = (mod, urls = new Set()) => {
-  if (!mod) return
-  if (!mod.url) return
-  if (mod.url.endsWith('.css') || (mod.id && /\?vue&type=style/.test(mod.id))) {
-    urls.add(mod.url)
-  }
-  mod.importedModules.forEach(dep => {
-    collectCssUrls(dep)
-  })
-  return urls
-}
-
 // use vite's connect instance as middleware
 server.use(viteServer.middlewares)
 server.use('*', async (req, res) => {
@@ -42,20 +28,8 @@ server.use('*', async (req, res) => {
     template = await viteServer.transformIndexHtml(url, template)
 
     const render = (await viteServer.ssrLoadModule('./src/entry-server.ts')).render
-    // const mod = await viteServer.moduleGraph.getModuleByUrl(
-    //   '/src/main.ts'
-    // ) /* TODO replace with your entry */
-    // const cssUrls = mod.ssrTransformResult.deps.filter(d => d.endsWith('.css'))
-    // const cssUrls = collectCssUrls(mod)
-    // console.log('cssUrls', cssUrls)
     const ctx = {}
-    const [appHtml, route, links] = await render(url, {}, ctx)
-    // console.log('88888', links)
-    const devCss = []
-      .map(url => {
-        return `<link rel="stylesheet" type="text/css" href="${url}">`
-      })
-      .join('')
+    const [appHtml, route] = await render(url, {}, ctx)
     const html = template
       .replace('<!--app-html-->', appHtml)
       .replace(`<title></title>`, `<title>${route.meta.title || ''}</title>`)
@@ -67,7 +41,6 @@ server.use('*', async (req, res) => {
         `<meta name="description" content="" />`,
         `<meta name="description" content="${route.meta.description || ''}" />`
       )
-      .replace('<!--dev-css-->', devCss)
     res.set({ 'Content-Type': 'text/html' }).end(html)
   } catch (e) {
     console.log(e)
