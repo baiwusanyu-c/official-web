18bc8a5dd3304b73b4f9200d2666d24c4a421264
gusheng <605661239@qq.com>
2022-07-27 18:21:48 +0800

    update: 打包测试

--- src/enums/config.ts
+++ src/enums/config.ts
@@ -1,33 +1,33 @@
 // 生产环境
 // import { IOption } from '../utils/types'
-
-enum productionUrl {
-  baseURL = 'https://beosin.com',
+const ssr = import.meta.env.SSR
+const productionUrl = {
+  baseURL: 'https://beosin.com',
 }
 // 开发环境
-enum developmentUrl {
+const developmentUrl = {
   // baseURL = 'http://192.168.0.216:9527',
-  baseURL = 'http://192.168.0.2:9527',
+  baseURL: 'http://192.168.0.2:9527',
 }
 // 测试环境
-enum stagingnUrl {
-  baseURL = 'http://192.168.1.231:9527',
+const stagingnUrl = {
+  baseURL: ssr ? 'http://192.168.1.231:9527' : 'http://124.71.132.90:9527',
 }
 // 预发布环境
-enum pretestnUrl {
-  baseURL = 'http://0.0.0.0:9527',
+const pretestnUrl = {
+  baseURL: 'http://0.0.0.0:9527',
 }
 
-enum developmentUrl02 {
-  baseURL = 'http://192.168.0.2:9527',
+const developmentUrl02 = {
+  baseURL: 'http://192.168.0.2:9527',
 }
 
-enum stagingnUrl02 {
-  baseURL = 'http://192.168.1.231:9527',
+const stagingnUrl02 = {
+  baseURL: ssr ? 'http://192.168.1.231:9527' : 'http://124.71.132.90:9527',
 }
 
-enum production02 {
-  baseURL = 'http://124.71.160.46:9527',
+const production02 = {
+  baseURL: 'http://124.71.160.46:9527',
 }
 const configUrl = {
   production: productionUrl,
