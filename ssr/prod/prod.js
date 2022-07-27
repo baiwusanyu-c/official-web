5605fae645724015872d14ddfc1dd01f7343db77
gusheng <605661239@qq.com>
2022-07-27 16:22:12 +0800

    update: 打包测试

--- ssr/prod/prod.js
+++ ssr/prod/prod.js
@@ -1,30 +1,29 @@
-const http = require('http')
 const fs = require('fs')
-// const serverStatic = require('serve-static')
 const { resolve } = require('path')
 const dir = resolve('../')
 const client = resolve(dir, './client')
 const server = resolve(dir, './server')
+const express = require('express')
+const app = express()
 
-// use vite's connect instance as middleware
-// server.use(serverStatic(client, { index: false }))
-http
-  .createServer(async (req, res) => {
+app
+  .use(express.static(client, { index: false }))
+  .use(async (req, res) => {
     try {
-      console.log(req.url)
-      const url = req.url
+      const url = req.originalUrl
       const template = fs.readFileSync(resolve(client, 'index.html'), 'utf-8')
       const { render } = require(resolve(server, './entry-server.js'))
       const manifest = require(resolve(client, './ssr-manifest.json'))
 
       const ctx = {}
       const html = await render(url, manifest, ctx, template)
-      res.setHeader('Content-Type', 'text/html')
+
+      res.set({ 'Content-Type': 'text/html' })
       res.end(html)
     } catch (e) {
       console.error(e)
     }
   })
-  .listen(9020, () => {
-    console.log('look at http://localhost:9020')
+  .listen(80, () => {
+    console.log('look at http://localhost:80')
   })
